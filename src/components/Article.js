edac3db2d4ab5eded73e42ee65de785cf3d0e63b
Gabrielle <ms.grodarte@gmail.com>
2024-10-01 14:44:08 -0700

    complete bonus

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,27 @@
 import React from "react";
 
-function Article({title, date="January 1, 1970", preview}){
+function Article({title, date="January 1, 1970", preview, minutes}){
+    function calculateMinutes(){
+        console.log(minutes)
+        let emojis = ""
+        if (minutes < 30){
+            const numEmojis = Math.ceil(minutes/5)
+            for(let i=1; i<=numEmojis; i++){
+                emojis += "☕️"
+            }
+        } else {
+            const numEmojis = Math.ceil(minutes/10)
+            for(let i=1; i<=numEmojis; i++){
+                emojis += "🍱"
+            }
+        }
+        return emojis
+    }
+
     return(
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} · {calculateMinutes()} {minutes} min read</small>
             <p>{preview}</p>
         </article>
     )
